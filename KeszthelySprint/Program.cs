d9ccd6f861142bd0373863c157da0f0b370a089e
Hunor17vok <163414670+Hunor17vok@users.noreply.github.com>
2024-12-09 13:57:03 +0100

    Add project files.

---
+++ KeszthelySprint/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzok> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyzok(sor));
+            }
+            Console.WriteLine($"2.feladat:Enyien versenyeztek{versenyzok.Count()}");
+
+            int a = 0;
+
+            foreach (var j in versenyzok)
+            {
+                if (j.Kategoria == "elit junior")
+                {
+                    a ++;
+                }
+            }
+            Console.WriteLine($"3.feladat:Enyien voltak elit juniorok: {a} fő");
+
+            //4
+            int össz_életkor = 0;
+            foreach (var v in versenyzok)
+            {
+                össz_életkor += DateTime.Now.Year - v.Szuletes;
+            }
+            Console.WriteLine($"4.feladat: Átlag életkor: {(össz_életkor / versenyzok.Count()):0.0} év");
+
+            //Console.WriteLine($"4.feladat: Átlag életkor: {Math.Round(össz_életkor / versenyzok.Count,1):0.0} év");
+        }
+    }
+}
---
+++ KeszthelySprint/Versenyzok.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeszthelySprint
+{
+    internal class Versenyzok
+    {
+        public string Nev { get; set; }
+        public int Szuletes { get; set; }
+        public int Rajtszám { get; set; }
+        public string Kategoria { get; set; }
+        public string Eletkor { get; set; }
+        public string Nem { get; set; }
+        public string ido { get; set; }
+        public string ido2 { get; set; }
+        public string ido3 { get; set; }
+        public string ido4 { get; set; }
+        public string ido5 { get; set; }
+        
+
+        public Versenyzok(string sor)
+        {
+            string[] A = sor.Split(';');
+            Nev = A[0];
+            Szuletes = int.Parse(A[1]);
+            Rajtszám = int.Parse(A[2]);
+            Kategoria = A[4];
+            Nem = A[3];
+            Eletkor = (A[5]);
+            ido = A[6];
+            ido2 = A[7];
+            ido3 = A[8];
+            ido4 = A[9];
+            
+        }
+    }
+}
